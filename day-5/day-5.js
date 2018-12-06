4ebff618a857e770ade93009f2d394781852ea18
Olivier Gauthier-Farley <olivier.gauthier-farley.1@ens.etsmtl.ca>
2018-12-06 13:28:34 -0500

    done

--- day-5/day-5.js
+++ day-5/day-5.js
@@ -1,15 +1,3 @@
-var startTime, endTime;
-
-function start() {
-  startTime = new Date();
-};
-
-function end() {
-  endTime = new Date();
-  var timeDiff = endTime - startTime; //in ms
-  console.log(timeDiff + " milliseconds");
-}
-
 function removeCharsAt(i,string){
   beginning = string.substring(0,i);
   ending = string.substring(i+2);
@@ -35,25 +23,18 @@ function findAllChars(data){
 
 function filterData(char,data){
   let uniCode = char.charCodeAt(0);
-  for(let i = 0; i < data.length;i++){
-    if(data.charCodeAt(i) === uniCode || data.charCodeAt(i) === (uniCode-32)){
-      data = removeCharAt(i,data);
-    }
-  }
-  return data;
+  let arrayData = data.split("");
+
+  arrayData = arrayData.filter(element =>{
+    return (element.charCodeAt(0) !== uniCode && element.charCodeAt(0) !== (uniCode-32))
+  })
+
+  return arrayData.join("");
 }
-
-
 
 part1 = (data) => {
   index = 0;
   end = data.length;
-
-  // // //32 || -32
-  // console.log(testing.charCodeAt(1)- testing.charCodeAt(0))
-
-  // test = "salutjesuiscool"
-  // console.log(removeCharsAt(5,test))
 
   while(index !== end){
     charDiff = Math.abs(data.charCodeAt(index) - data.charCodeAt(index+1));
@@ -67,40 +48,29 @@ part1 = (data) => {
       end = data.length;
       index++;
     }
-    return data.length;
   }
+  return data.length;
 }
 
-part2 =()=>{
-  var fs = require('fs');
-  var data = fs.readFileSync('./test.txt', 'utf8');
-
-  var lowerCasesData = data.toLowerCase();
-  var lowerCaseCharTypes = findAllChars(lowerCasesData);
+part2=()=>{
+  let fs = require('fs');
+  let data = fs.readFileSync('./input.txt', 'utf8');
+  let lowerCasesData = data.toLowerCase();
+  let lowerCaseCharTypes = findAllChars(lowerCasesData);
 
   let minString = data.length;
+  let dataFiltered;
 
-  console.log(data)
-  console.log("--------------");
   for(let i = 0; i < lowerCaseCharTypes.length; i++){
 
-    dataFiltered = filterData(lowerCaseCharTypes[i],data,lowerCasesData);
+    dataFiltered = filterData(lowerCaseCharTypes[i],data);
     newDataLength = part1(dataFiltered);
 
-    console.log(lowerCasesData)
-    console.log(lowerCaseCharTypes[i]);
-    console.log(dataFiltered);
-    console.log("**************");
-
-
     if(newDataLength < minString){
-      minString = newDataLength.length;
+      minString = newDataLength;
     }
   }
-
   console.log(minString)
-
 }
-
 
 part2();
