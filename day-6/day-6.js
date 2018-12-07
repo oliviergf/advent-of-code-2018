70adc2b88b3738d9a1d7d115f6490b7be2c9f2ea
Olivier Gauthier-Farley <olivier.gauthier-farley.1@ens.etsmtl.ca>
2018-12-06 23:42:50 -0500

    part 2 done!

--- day-6/day-6.js
+++ day-6/day-6.js
@@ -4,14 +4,30 @@ let data = fs.readFileSync('./input.txt', 'utf8');
 findBoardSize =(data)=>{
   maxI = 1;
   maxJ = 1;
-
   for(let i = 0; i < data.length; i++){
     data[i] = data[i].replace(" ","");
     data[i] =  data[i].split(",");
-
     let x = parseInt(data[i][0]);
     let y = parseInt(data[i][1]);
 
+    if(x > maxI){
+      maxI = x;
+    }
+    if(y > maxJ){
+      maxJ = y;
+    }
+  }
+  coord = [maxI,maxJ];
+  return coord;
+}
+
+findBoardSizePart2=(data)=>{
+  maxI = 1;
+  maxJ = 1;
+
+  for(let i = 0; i < data.length; i++){
+    let x = parseInt(data[i][0]);
+    let y = parseInt(data[i][1]);
     if(x > maxI){
       maxI = x;
     }
@@ -53,6 +69,20 @@ findNearest=(i,j,data) =>{
     return (".");
 }
 
+findClosest=(i,j,data) =>{
+  //find distance
+  totalDistances = [];
+  for(let n = 0; n < data.length; n++){
+    totalDistances.push(Math.abs(j - (parseInt(data[n][0])))+(Math.abs(i - (parseInt(data[n][1])))));
+  }
+  sum = 0;
+  //calculates the total distance to all points
+  for(let n = 0; n < totalDistances.length; n++){
+    sum = sum + totalDistances[n];
+  }
+  return sum;
+}
+
 findInfinites = (board) =>{
   listOfExcludes = [];
 
@@ -82,10 +112,6 @@ findInfinites = (board) =>{
   return listOfExcludes;
 }
 
-calculateLargestArea = (board,listofInfinites) =>{
-
-}
-
 part1 = () =>{
   data = data.split("\n")
   elements = new Map();
@@ -96,11 +122,10 @@ part1 = () =>{
   }
 
   elements.set('.',0);
-
   maxSize = findBoardSize(data);
 
+  //builds the board
   let board = [];
-
   for(let i = 0; i <= maxSize[1]; i++){
     let array = [];
     for(let j = 0; j <= maxSize[0]; j++){
@@ -109,10 +134,12 @@ part1 = () =>{
     board.push(array);
   }
 
+  //if the point is touching the edge, it's group will be infinite.
   listofInfinites = findInfinites(board);
 
   calculateLargestArea(board,listofInfinites);
 
+  //calculates the number of occurents of each possible value.
   for(let i = 0; i < board.length; i++){
     for(let j = 0 ; j< board[0].length; j++){
       if(!listofInfinites.includes(board[i][j])){
@@ -122,18 +149,48 @@ part1 = () =>{
       }
     }
   }
-  let maxCount = 0;
 
+  //finds the biggest field of valid value
+  let maxCount = 0;
   elements.forEach((value, key,)=>{
     if(value > maxCount){
       maxCount = value;
     }
   });
-  console.log(maxCount);
+  console.log("part 1 : "+ maxCount.toString());
 }
 
 part2=()=>{
-  
+  elements = new Map();
+
+  //find all elements
+  for(let i = 0; i < data.length; i++){
+    elements.set(i,0);
+  }
+  elements.set('.',0);
+  maxSize = findBoardSizePart2(data);
+
+  //builds the board with the total distance to all spots of the point.
+  let board = [];
+  for(let i = 0; i <= maxSize[1]; i++){
+    let array = [];
+    for(let j = 0; j <= maxSize[0]; j++){
+      array.push(findClosest(i,j,data));
+    }
+    board.push(array);
+  }
+
+  //calculate the total amount of points with a distance less than 10000
+  count = 0;
+  for(let i = 0; i < board.length; i++){
+    for(let j = 0 ; j< board[0].length; j++){
+      if(board[i][j] < 10000){
+        count++;
+      }
+    }
+  }
+
+  console.log("part 2 :" + count.toString())
 };
 
 part1();
